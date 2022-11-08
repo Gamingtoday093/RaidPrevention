537c505f841ce2ea05441ed625113ce94720cd07
Gamingtoday093 <Gamingtoday093@gmail.com>
2022-11-08 23:45:52 +0100

    Bug Fixes

--- RaidPrevention/RaidPrevention.cs
+++ RaidPrevention/RaidPrevention.cs
@@ -63,9 +63,9 @@ namespace RaidPrevention
 
                 if (playerExists && player.IsAdmin && Config.AdminByPass) return;
                 if (playerExists && ((IRocketPlayer)player).HasPermission(Config.ByPassPermission) && !player.IsAdmin) return;
-                if (Config.AllowSelfDestruction && playerExists && (ulong)instigatorSteamID == structure.GetServersideData().owner) return;
-                if (Config.AllowGroupDestruction && playerExists && (ulong)player.SteamGroupID == structure.GetServersideData().group && player.SteamGroupID != null) return;
-                if (Config.AllowGroupDestruction && playerExists && (ulong)GroupManager.getGroupInfo(player.CSteamID)?.groupID == structure.GetServersideData().group) return;
+                if (Config.AllowSelfDestruction && playerExists && instigatorSteamID.m_SteamID == structure.GetServersideData().owner) return;
+                if (Config.AllowGroupDestruction && playerExists && player.SteamGroupID.m_SteamID == structure.GetServersideData().group && structure.GetServersideData().group != 0) return;
+                if (Config.AllowGroupDestruction && playerExists && GroupManager.getGroupInfo(player.CSteamID)?.groupID.m_SteamID == structure.GetServersideData().group) return;
 
                 if (Config.OnlyPreventDestruction && structure.GetServersideData().structure.health > pendingTotalDamage) return;
 
@@ -100,7 +100,7 @@ namespace RaidPrevention
         {
             if (isBattle && localBattle == null || isBattle && localBattle != null && (barricadeTransform.position - localBattle.Point).sqrMagnitude <= Mathf.Pow(localBattle.Radius, 2)) return;
             if (pendingTotalDamage <= 0) return;
-            if (damageOrigin == EDamageOrigin.Plant_Harvested) return;
+            if (damageOrigin == EDamageOrigin.Plant_Harvested || damageOrigin == EDamageOrigin.Charge_Self_Destruct || damageOrigin == EDamageOrigin.Horde_Beacon_Self_Destruct) return;
 
             if (!Config.IsBlacklist && Config.BarricadeStructureIDs.Any(x => x.ToString() == barricadeTransform.name) || Config.IsBlacklist && !Config.BarricadeStructureIDs.Any(x => x.ToString() == barricadeTransform.name))
             {
@@ -116,9 +116,9 @@ namespace RaidPrevention
 
                 if (barricade.asset.build == EBuild.FARM)
                 {
-                    if (Config.AllowSelfDestruction && playerExists && (ulong)instigatorSteamID == barricade.GetServersideData().owner) return;
-                    if (Config.AllowGroupDestruction && playerExists && (ulong)player.SteamGroupID == barricade.GetServersideData().group && player.SteamGroupID != null) return;
-                    if (Config.AllowGroupDestruction && playerExists && (ulong)GroupManager.getGroupInfo(player.CSteamID)?.groupID == barricade.GetServersideData().group) return;
+                    if (Config.AllowSelfDestruction && playerExists && instigatorSteamID.m_SteamID == barricade.GetServersideData().owner) return;
+                    if (Config.AllowGroupDestruction && playerExists && player.SteamGroupID.m_SteamID == barricade.GetServersideData().group && barricade.GetServersideData().group != 0) return;
+                    if (Config.AllowGroupDestruction && playerExists && GroupManager.getGroupInfo(player.CSteamID)?.groupID.m_SteamID == barricade.GetServersideData().group) return;
 
                     pendingTotalDamage = 0;
                     shouldAllow = false;
@@ -146,9 +146,9 @@ namespace RaidPrevention
                     return;
                 }
 
-                if (Config.AllowSelfDestruction && playerExists && (ulong)instigatorSteamID == barricade.GetServersideData().owner) return;
-                if (Config.AllowGroupDestruction && playerExists && (ulong)player.SteamGroupID == barricade.GetServersideData().group && player.SteamGroupID != null) return;
-                if (Config.AllowGroupDestruction && playerExists && (ulong)GroupManager.getGroupInfo(player.CSteamID)?.groupID == barricade.GetServersideData().group) return;
+                if (Config.AllowSelfDestruction && playerExists && instigatorSteamID.m_SteamID == barricade.GetServersideData().owner) return;
+                if (Config.AllowGroupDestruction && playerExists && player.SteamGroupID.m_SteamID == barricade.GetServersideData().group && barricade.GetServersideData().group != 0) return;
+                if (Config.AllowGroupDestruction && playerExists && GroupManager.getGroupInfo(player.CSteamID)?.groupID.m_SteamID == barricade.GetServersideData().group) return;
 
                 if (Config.OnlyPreventDestruction && barricade.GetServersideData().barricade.health > pendingTotalDamage) return;
 
@@ -182,9 +182,9 @@ namespace RaidPrevention
 
             if (!Config.IsBlacklist && Config.BarricadeStructureIDs.Contains(crop.asset.id) || Config.IsBlacklist && !Config.BarricadeStructureIDs.Contains(crop.asset.id))
             {
-                if (Config.AllowSelfDestruction && (ulong)player.CSteamID == crop.GetServersideData().owner) return;
-                if (Config.AllowGroupDestruction && (ulong)player.SteamGroupID == crop.GetServersideData().group && player.SteamGroupID != null) return;
-                if (Config.AllowGroupDestruction && (ulong)GroupManager.getGroupInfo(player.CSteamID)?.groupID == crop.GetServersideData().group) return;
+                if (Config.AllowSelfDestruction && player.CSteamID.m_SteamID == crop.GetServersideData().owner) return;
+                if (Config.AllowGroupDestruction && player.SteamGroupID.m_SteamID == crop.GetServersideData().group && crop.GetServersideData().group != 0) return;
+                if (Config.AllowGroupDestruction && GroupManager.getGroupInfo(player.CSteamID)?.groupID.m_SteamID == crop.GetServersideData().group) return;
 
                 shouldAllow = false;
                 UnturnedChat.Say(player, Translate("PreventedDestruction"), MessageColour);
